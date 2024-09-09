0d4092695e09666d1022d084d3ec11dd29edb6f0
MitchellBaxter <baxtermitc@myvuw.ac.nz>
2024-09-09 19:27:55 +1200

    sunset

--- music_starter.js
+++ music_starter.js
@@ -6,8 +6,8 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if (counter==0){
 palmtree = loadImage("Palmtree.png")
   }
-  background(255, 236, 180); // cream
-  fill(235, 205, 52); // purple
+  background(235, 155, 52); // cream
+  fill(235, 140, 52); // purple
 
   let stripeWidth = map(other, 5, 100, 5, 20, true);
 
@@ -23,13 +23,13 @@ palmtree = loadImage("Palmtree.png")
   rect(0, height - 300, width, 300);
 
   let ovalPlace = map(vocal, 20, 100, height - 430, 100, true);
-  let ovalSize = map(vocal, 80, 100, 240, 150, true);
+  let ovalSize = map(vocal, 80, 100, 200, 100, true);
   
   // Outer glow effect
   for (let i = 0; i < 10; i++) {
     let glowSize = ovalSize + i * 10; // Increase the size for each glow layer
     let glowAlpha = 255 - i * 25; // Decrease the opacity for each glow layer
-    fill(235, 140, 52, glowAlpha); // Yellow with varying opacity
+    fill(235, 120, 52, glowAlpha); // Yellow with varying opacity
     ellipse(width / 2, ovalPlace, glowSize);
   }
   
@@ -49,6 +49,7 @@ palmtree = loadImage("Palmtree.png")
   last_words = words;
 
   
+  
 //Water
  fill(66, 135, 245);
 rect(0, height -400, width, 150);
@@ -57,27 +58,29 @@ rect(0, height -400, width, 150);
 //Hill
 beginShape();
 fill(38, 148, 50)
-vertex(0, 470);
-bezierVertex(272, 583, 500, 300, 755, 496);
-bezierVertex(783, 519, 804, 527, 826, 529);
-bezierVertex(849, 529, 881, 529, 898, 529);
-bezierVertex(904, 555, 916, 608, 914, 679);
-bezierVertex(911, 710, 843, 713, 724, 709);
-bezierVertex(615, 706, 427, 713, 272, 708);
-bezierVertex(72, 707, 4, 706, 0, 701);
-bezierVertex(0, 651, 0, 567, 0, 470);
+vertex(-51, 532);
+bezierVertex(201, 668, 500, 300, 916, 601);
+bezierVertex(916, 642, 912, 676, 909, 711);
+bezierVertex(845, 714, 809, 705, 718, 707);
+bezierVertex(597, 709, 470, 707, 321, 712);
+bezierVertex(182, 708, 86, 708, 4, 708);
+bezierVertex(0, 640, 0, 640, 0, 640);
 endShape();
 
+
+
 // Adjust image size based on drum value
-let imageSize = map(drum, 0, 40, 400, 800); // Adjust the range as needed
+let imageSize = map(drum, -150, 40, 100, 800); // Adjust the range as needed
 let imageX = 625; // Initial X position
-let imageY = 200; // Initial Y position
+let imageY = 100; // Initial Y position
+
 
 image(palmtree, imageX, imageY, imageSize, imageSize);
 push();
 scale(-1, 1);
-image(palmtree, -275, 200, imageSize, imageSize);
+image(palmtree, -275, 100, imageSize, imageSize);
 pop();
+
 
   textFont('Georgia');
   textAlign(CENTER);
