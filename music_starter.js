5f10fe785373458ce3ba822e5a1aa7e602356014
MitchellBaxter <baxtermitc@myvuw.ac.nz>
2024-09-12 10:14:22 +1200

    wave
    Grass and wave effect

--- music_starter.js
+++ music_starter.js
@@ -1,10 +1,16 @@
 let last_words = "";
 let last_words_opacity = 0;
 
+
 let palmtree;
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if (counter==0){
 palmtree = loadImage("Palmtree.png")
+  }
+
+
+  if (counter==0){
+Grass = loadImage("Grass.png")
   }
   background(235, 155, 52); // cream
   fill(235, 140, 52); // purple
@@ -18,12 +24,8 @@ palmtree = loadImage("Palmtree.png")
 
 }
 
-  // Beach
-  fill(237, 201, 175); 
-  rect(0, height - 300, width, 300);
-
-  let ovalPlace = map(vocal, 20, 100, height - 430, 100, true);
-  let ovalSize = map(vocal, 80, 100, 200, 100, true);
+  let ovalPlace = map(vocal, 20, 100, height - 400, 100, true);
+  let ovalSize = map(vocal, 80, 100, 150, 120, true);
   
   // Outer glow effect
   for (let i = 0; i < 10; i++) {
@@ -49,15 +51,18 @@ palmtree = loadImage("Palmtree.png")
   last_words = words;
 
   
-  
-//Water
- fill(66, 135, 245);
-rect(0, height -400, width, 150);
+//Water with bass effect
+fill(66, 100, 230);
+let waveHeight = map(bass, -100, 150, 10, 40); // Adjust the range and intensity as needed
+for (let i = 0; i < width; i += 10) {
+  let waveOffset = map(sin(frameCount * 1.2 + i * 0.1), -1, 1, -waveHeight, waveHeight); 
+  rect(i, height - 400 + waveOffset, 10, 300 - waveOffset);
+}
 
 
 //Hill
 beginShape();
-fill(38, 148, 50)
+fill(235, 140, 52)
 vertex(-51, 532);
 bezierVertex(201, 668, 500, 300, 916, 601);
 bezierVertex(916, 642, 912, 676, 909, 711);
@@ -66,7 +71,6 @@ bezierVertex(597, 709, 470, 707, 321, 712);
 bezierVertex(182, 708, 86, 708, 4, 708);
 bezierVertex(0, 640, 0, 640, 0, 640);
 endShape();
-
 
 
 // Adjust image size based on drum value
@@ -82,7 +86,7 @@ image(palmtree, -275, 100, imageSize, imageSize);
 pop();
 
 
-  textFont('Courier New');
+  textFont('Pacifico');
   textAlign(CENTER);
   textStyle(BOLD);
   textSize(50);
@@ -90,9 +94,9 @@ pop();
   fill(0, 0, 0, int(last_words_opacity));
   text(words, width / 2, height / 1.85);
 
-
-
-
+//grass image
+image(Grass, -200,550,800,150)
+image(Grass,200, 550,800,150)
 
 
 
